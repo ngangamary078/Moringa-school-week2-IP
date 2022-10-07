12ed5f87710c557c2dd272b7db55d713310646a6
Mary <ngangamary078@gmail.com>
2022-10-07 19:05:19 +0300

    inserted some content and delete some

--- js/JSmain.js
+++ js/JSmain.js
@@ -1,227 +1,49 @@
 var CC, YY, MM, DD, d, dayValue;
 
-var dayWeekNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+const dayWeekNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 
-var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+const maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-function validate() {
+var Century = 19;
+var year =1989;
+var month =12;
+var day =5;
+var gender = 'm';
 
-  var genders = document.getElementsByName("gender");
+function calculatorNameAndDay(Century, year, month, day, geder){
+  var idexOfTheDay =math.round((((Century/4) -2*Century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + Date ) % 7)
 
-  if( 
-    document.myForm.year.value == "" || 
-    document.myForm.year.value.length !=4 || 
-    document.myForm.year.value >2100 || 
-    document.myForm.year.value <=1900
-    ) {
-
-     alert( "Please provide a valid year of birth! eg 2019" );
-
-     document.myForm.year.focus() ;
-
-     return false;
-
+  var dayTheUserWasBorn = DAYS_OF_THE_WEEK [idexOfTheDay]
+  varname = null;
+  if (gender === 'm'){
+    MALE_NAMES
+  }else if (geder === 'f'){
+    name = FEMALE_NAME [idexOfTheDay];
   }
-
-  else if
-  ( 
-    document.myForm.month.value == "" || isNaN( document.myForm.month.value ) ||
-
-    document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
-
-     alert( "Please provide your month of birth! between 1 and 12" );
-
-     document.myForm.month.focus() ;
-
-     return false;
-
-  }
-
-  else if( 
-  document.myForm.date.value == "" || isNaN( document.myForm.month.value ) ||
-  document.myForm.month.value.length != 2|| 
-  document.myForm.date.value > 31 || 
-  document.myForm.date.value <= 0) {
-
-     alert( "Please provide a valid date that you were born in!" );
-
-     document.myForm.day.focus() ;
-
-     return false;
-
-  }
-
-  else if(genders[0].checked==false && genders[1].checked==false ) {
-
-      alert("You must select male or female");
-
-      return false;
-
-  }  
-
+  
   else{
-
-    return true ;
-
+    alert("Invalid gender");
   }
+  
+  var list = [dayTheUserWasBorn, name];
+  return list;
 
- 
+
+
 
 }
 
- 
-
-function calculateDayValue(){
-
-  year = document.getElementById("year").value;
-
-  CC = parseInt(year.substring(0,2));
-
-  YY = parseInt(year.substring(2,4));
-
-  MM = parseInt(document.getElementById("month").value);
-
-  DD = parseInt(document.getElementById("date").value);
-
-  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-
-  console.log(d);
-
-  return (Math.floor(d));
+function JSmain(){
+var userInputyear =parseInt(prompt("Enter the year"));
+var userInputmonth =parseInt(prompt("Enter the month"));
+var userInputday =parseInt(prompt("Enter the day"));
+var userInputgender =parseInt("Enter the gender");
+var userInputCentury =parseInt( UserIputYear.toString().substring(0, 2));
+var output = calcuteNameAndDay(userInputCentury, userInputyear, userInputmonth, userInputday, userInputgender);
+var outputElement = document.getElementById("output");
+outputElement.innerHTML = "You were born on: "+ output[0] +".Your Akan name is:"+ output[1];
 
 }
 
- 
-
-function getGender(){
-
-  var genders = document.getElementsByName("gender");
-
-  if(genders[0].checked == true){
-
-    var gender = "male";
-
-  }
-
-  else if(genders[1].checked == true){
-
-    var gender = "female";
-
-  }
-
-  else{
-
-    return false;
-
-  }
-
-  switch(gender){
-
-    case "male":
-
-      if (dayValue == 1){
-
-        alert("You were born on " +dayWeekNames[0] + " and Your akan name is " +maleAkanNames[0]+"!");
-
-      }
-
-      else if(dayValue == 2){
-
-        alert("You were born on "+dayWeekNames[1] + " and Your akan name is " +maleAkanNames[1]+"!");
-
-      }
-
-      else if(dayValue == 3){
-
-        alert("You were born on " +dayWeekNames[2]+ " and Your akan name is " +maleAkanNames[2]+"!");
-
-      }
-
-      else if(dayValue == 4){
-
-        alert("You were born on "+dayWeekNames[3] +  " and Your akan name is " +maleAkanNames[3]+"!");
-
-      }
-
-      else if(dayValue == 5){
-
-        alert("You were born on "+dayWeekNames[4] +  " and Your akan name is " +maleAkanNames[4]+"!");
-
-      }
-
-      else if(dayValue == 6){
-
-        alert("You were born on "+dayWeekNames[5] +  " and Your akan name is " +maleAkanNames[5]+"!");
-
-      }
-
-      else if(dayValue == -0){
-
-        alert("You were born on "+dayWeekNames[6] + " and Your akan name is " +maleAkanNames[6]+"!");
-
-      }
-
-    break;
-
-    case "female":
-
-      if (dayValue == 1){
-
-        alert("You were born on "+dayWeekNames[0] + " and Your akan name is  " +femaleAkanNames[0]+"!");
-
-      }
-
-      else if(dayValue == 2){
-
-        alert("You were born on " +dayWeekNames[1] + " and Your akan name is " +femaleAkanNames[1]+"!");
-
-      }
-
-      else if(dayValue == 3){
-
-        alert("You were born on " +dayWeekNames[2] + " and Your akan name is " +femaleAkanNames[2]+"!");
-
-      }
-
-      else if(dayValue == 4){
-
-        alert("You were born on " +dayWeekNames[3] + " and Your akan name is " +femaleAkanNames[3]+"!");
-
-      }
-
-      else if(dayValue == 5){
-
-        alert("You were born on " +dayWeekNames[4] + " and Your akan name is " +femaleAkanNames[4]+"!");
-
-      }
-
-      else if(dayValue == 6){
-
-        alert("You were born on " +dayWeekNames[5] + " and Your akan name is " + femaleAkanNames[5]+"!");
-
-      }else if(dayValue == -0){
-
-        alert("You were born on " +dayWeekNames[6] + " and Your akan name is " +femaleAkanNames[6]+"!");
-
-      }
-
-    break
-
-    default:
-
-         
-
-  }
-
-}
-
-function findName(){
-
-  dayValue = calculateDayValue();
-
-  getGender();
-
-}
-alert
