c88aa3cae6d8cc329161b5c6b9968317735a6164
Mary <ngangamary078@gmail.com>
2022-10-09 12:28:27 +0300

    added and deleted insertion

--- js/JSmain.js
+++ js/JSmain.js
@@ -1,51 +1,57 @@
-var CC, YY, MM, DD, d, dayValue;
+function akanNames() {
+  /*OUR WORKING LISTS */
+  var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; //List of days of the week
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //List of female names
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //List of male names
 
-const dayWeekNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+  /* USER PROMPS */
+  var dateInput = prompt("Enter date of birth (YYYY-MM-DD):");
 
-const maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+  /*DATE MANIPULATION VARIABLES*/
+  var birthDate = new Date(dateInput);//uses the variable dateInputfrom the user to create a caleder date
+  var dateyenyewe = birthDate.getDay();//pick the day from the date object
+  var birthMonth = birthDate.getMonth();//picks the month from the object
+  var birthYear = birthDate.getFullYear();// picks the month from the object
 
-var Century = 19;
-var year =1989;
-var month =12;
-var day =5;
-var gender = 'm';
+  /* USING THE DATES TO ACCESS THE NAM LIST*/
+  var genderFemale = femaleNames[dateyenyewe];
+  var genderMale = maleNames[dateyenyewe];
 
-function calculatorNameAndDay(Century, year, month, day, geder){
-  var idexOfTheDay =math.round((((Century/4) -2*Century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + Date ) % 7)
+  /*CAPTURE WRONG DATE INPUTS*/
+  if (
+      dateyenyewe >= 0 &&
+      dateyenyewe <= 7 &&
+      birthMonth >= 0 &&
+      birthMonth <= 12 &&
+      birthYear >= 1000
+  ) {
+      var sex = prompt("Enter your Gender (M/F):").toUpperCase(); //takes the user gender and convert it to toUpperCase
+      /* to capture wrong gender choice */
 
-  var dayTheUserWasBorn = DAYS_OF_THE_WEEK [idexOfTheDay]
-  varname = null;
-  if (gender === 'm'){
-    MALE_NAMES
-  }else if (geder === 'f'){
-    name = FEMALE_NAME [idexOfTheDay];
+      if (sex === "M" || sex === "F" || sex === "MALE" || sex === "FEMALE") {
+
+          if (sex === "M") {
+              document.getElementById("myFeedback").innerHTML="Your Akan name is " + genderMale;
+              // alert("Your Akan name is " + genderMale);
+          }
+          else {
+              document.getElementById("myFeedback").innerHTML="Your Akan name is " + genderFemale;            
+              
+
+              // alert("Your Akan name is " + genderFemale);
+
+          }
+      }
+      else {
+          alert("Invalid Gender use F/M");
+      }
   }
-  
-  else{
-    alert("Invalid gender");
+
+  else {
+      alert("WRONG DATE FORMAT. PLEASE USE YYYY-MM-DD\nExample: 2022-10-08");
   }
-  
-  var list = [dayTheUserWasBorn, name];
-  return list;
-
-
-
-
 }
 
-function JSmain(){
-var userInputyear =parseInt(prompt("Enter the year"));
-var userInputmonth =parseInt(prompt("Enter the month"));
-var userInputday =parseInt(prompt("Enter the day"));
-var userInputgender =parseInt("Enter the gender");
-var userInputCentury =parseInt( UserIputYear.toString().substring(0, 2));
-var output = calcuteNameAndDay(userInputCentury, userInputyear, userInputmonth, userInputday, userInputgender);
-var outputElement = document.getElementById("output");
-outputElement.innerHTML = "You were born on: "+ output[0] +".Your Akan name is:"+ output[1];
-
-outputElement.classList.add("akan.name");
-}
 
 
